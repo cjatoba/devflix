e7ebf26e508b402a9ffaad12a5a281f151cf9075
clayton jatoba dos santos <cjsantos@saovicente.intra>
2020-07-29 17:47:28 -0300

    Aula 2 parte 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home';
+import CadastroVideo from './pages/Cadastro/Video';
+import Erro404 from './pages/Erro404';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroCategoria from './pages/Cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Erro404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/Erro404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Erro404(){
+    return (
+        <div>
+            Pgina não localizada - Erro 404
+        </div>
+    )
+}
+
+export default Erro404;
