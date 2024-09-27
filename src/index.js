9044ba15885b3b1d65da1474ed11c249f863e5f2
Clayton Jatobá <52887959+cjatoba@users.noreply.github.com>
2024-09-27 11:30:13 +0000

    update packages

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './pages/Home';
 import CadastroVideo from './pages/Cadastro/Video';
 import Erro404 from './pages/Erro404';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CadastroCategoria from './pages/Cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} exact/>
-      <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      <Route component={Erro404} />
-    </Switch>
+    <Routes>
+      <Route path="/" exact element={<App />} />
+      <Route path="/cadastro/video" element={<CadastroVideo />} />
+      <Route path="/cadastro/categoria" element={<CadastroCategoria />} />
+      <Route element={<Erro404 />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
